2b3b8288dabdb6d3fc9f05342de00b30e59df77f
SuBinKim <50948148+KIMSUBIN17@users.noreply.github.com>
2021-08-10 19:16:13 +0900

    Update 6-3 삽입 정렬.py

--- Chap06 정렬/6-3 삽입 정렬.py
+++ Chap06 정렬/6-3 삽입 정렬.py
@@ -2,7 +2,7 @@ array = [7,5,9,0,3,1,6,2,4,8]
 #print("정렬 전", array)
 
 for i in range(1, len(array)):
-  for j in range(i,0,-1): # 인덱스i부터 1까지 감소하며 반복
+  for j in range(i,0,-1): # 인덱스i부터 1까지 1씩 감소하면서 반복
     if array[j] < array[j-1] : # 한 칸씩 왼쪽으로 이동
       array[j], array[j-1] = array[j-1], array[j]
   else: #자기보다 작은 데이터를 만나면 그 위치에서 멈춤
@@ -10,3 +10,5 @@ for i in range(1, len(array)):
 
 
 print("정렬 후", array)
+
+#range함수에서 마지막 숫자는 range에 지정한 끝나는 숫자보다 1이 더 큼
