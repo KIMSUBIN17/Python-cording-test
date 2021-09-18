7a6470d04ef29f46ffb980a8b660c875d09f4bca
KIMSUBIN17 <tnqls24860@naver.com>
2021-09-18 23:44:56 +0900

    Create 6-10 위에서 아래로.py

---
+++ Chap06 정렬/6-10 위에서 아래로.py
@@ -0,0 +1,19 @@
+#크기 상관없이 나열되어 있는 수열을 큰 수부터 작은 수의 순서로 정렬
+#수열을 내림차순으로 정렬하는 프로그램
+
+#기본적인 정렬문제
+#수열에 속해있는 수의 개수가 500개 이하이므로 적은편이므로 코드가 간단한 파이썬 기본정렬 라이브러리 sorted()사용
+
+n = int(input())
+
+#n개의 정수 입력받아 리스트에 저장
+array = []
+for i in range(n):
+  array.append(int(input()))
+
+#파이썬 기본 정렬 라이브러리로 정렬 수행
+array = sorted(array, reverse=True)
+
+#정렬 결과 출력
+for i in array:
+  print(i, end=' ')
